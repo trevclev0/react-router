cc82bef986114c9b94a1bf9cd525282829f6446e
Trevor Duurán <trevor.m.duran@gmail.com>
2024-08-29 10:40:11 -0600

    chore: proper casing for NavBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useMemo } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Navbar from '../Navbar/Navbar';
 import Home from '../../pages/Home';
 import Profile from '../../pages/Profile';
 import Contact from '../../pages/Contact';
 import AppContext from '../../context/AppContext';
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   const [username, setUsername] = useState('Trevor');
@@ -24,7 +24,7 @@ function App() {
       <AppContext.Provider value={contextValue}>
         <QueryClientProvider client={client}>
           <Router>
-            <Navbar />
+            <NavBar />
             <Routes>
               <Route path="/" element={<Home username={username} />} />
               <Route path="/profile" element={<Profile username={username} />} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,9 @@
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+export default function NavBar() {
   return (
-    <nav>
+    <nav className="NavBar">
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/profile">Profile</Link></li>
